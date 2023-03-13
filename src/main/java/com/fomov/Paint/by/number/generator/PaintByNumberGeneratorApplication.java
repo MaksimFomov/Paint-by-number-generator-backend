da77959f4b262fa45e6d636da2e8e2e6f3bba844
Maxim Fomov <maksimfomov26@gmail.com>
2023-03-13 13:38:07 +0300

    init: Project generated

---
+++ src/main/java/com/fomov/Paint/by/number/generator/PaintByNumberGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.fomov.Paint.by.number.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintByNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintByNumberGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fomov/Paint/by/number/generator/PaintByNumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fomov.Paint.by.number.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintByNumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
